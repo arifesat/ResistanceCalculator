46e67aa02206c083c8bafee001377aa706c2dd44
Arif Esat Yilmaz <arif_esat@outlook.com>
2023-10-16 12:07:12 +0300

    added calculation functionality

--- index.js
+++ index.js
@@ -1,42 +1,38 @@
-const blackValue = 0;
-const brownValue = 1;
-const redValue = 2;
-const orangeValue = 3;
-const yellowValue = 4;
-const greenValue = 5;
-const blueValue = 6;
-const purpleValue = 7;
-const greyValue = 8;
-const whiteValue = 9;
-const goldValue = -1;
-const silverValue = -2;
 
-const brownTolerance = 1/100;
-const redTolerance = 2/100;
-const greenTolerance = 5/1000;
-const blueTolerance = 25/10000;
-const purpleTolerance = 1/1000;
-const greyTolerance = 5/10000;
-const goldTolerance = 5/100;
-const silverTolerance = 1/10;
+const TOLERANCE_VALUES = {
+    brown: 1/100,
+    red: 2/100,
+    green: 5/1000,
+    blue: 25/10000,
+    violet: 1/1000,
+    gray: 5/10000,
+    gold: 5/100,
+    silver: 1/10,
+  };
 
-let stripe1 = 0;
-let stripe2 = 0;
-let stripe3 = 0;
-let stripe4 = 0;
-let stripe5 = 0;
+let stripe1 = null;
+let stripe2 = null;
+let stripe3 = null;
+let stripe4 = null;
+let stripe5 = null;
+
+let stripe4Tolerance = null;
+let stripe5Tolerance = null;
 
 const selectionListItems = document.querySelectorAll('.colorPicker li');
 
 selectionListItems.forEach(item => {
     item.addEventListener('click', function () {
-        let lowerContent = item.textContent.toLowerCase()
+        let selectedColor = item.textContent.toLowerCase()
         let stripeNumber = item.className;
-        let newStripeURL = 'url("images/' + lowerContent + 'Stripe.png")'
+        let newStripeURL = 'url("images/' + selectedColor + 'Stripe.png")'
         document.getElementById(stripeNumber).style.backgroundImage = (newStripeURL);
         let resistanceValue = item.getAttribute('value')
         // console.log(resistanceValue);
         // console.log(stripeNumber);
+
+        stripe4Tolerance = TOLERANCE_VALUES[selectedColor]
+        stripe5Tolerance = TOLERANCE_VALUES[selectedColor]
 
         switch(stripeNumber) {
             case 'first':
@@ -62,10 +58,36 @@ selectionListItems.forEach(item => {
             default:
                 break;
             }
-            
+              
     });
 });
 
 function calculateResistance () {
     
+    if (stripe5 === null) {
+        baseResistance = parseInt(stripe1.toString() + stripe2.toString()) * Math.pow(10, stripe3);
+        let toleranceValue = stripe4Tolerance;
+        let resistanceMax = baseResistance + (baseResistance * toleranceValue);
+        let resistanceMin = baseResistance - (baseResistance * toleranceValue);
+        let mainResistance = baseResistance;
+        console.log(mainResistance);
+        console.log(resistanceMin + '< ' + mainResistance + ' < ' + resistanceMax);
+    }
+
+    else {
+        baseResistance = parseInt(stripe1.toString() + stripe2.toString() + stripe3.toString()) * Math.pow(10, stripe4);
+        let toleranceValue = stripe5Tolerance;
+        let resistanceMax = baseResistance + (baseResistance * toleranceValue);
+        let resistanceMin = baseResistance - (baseResistance * toleranceValue);
+        let mainResistance = baseResistance;
+        console.log(mainResistance);
+        console.log(resistanceMin + '< ' + mainResistance + ' < ' + resistanceMax);
+    }
+
+    
 };
+
+document.getElementById('calculateButton').addEventListener('click', function() {
+    calculateResistance();
+});
+
